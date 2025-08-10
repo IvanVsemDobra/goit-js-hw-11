9ddfc210e8ec9eb1a08b128c01c1b6fb2ab15376
VsemDobra <ivanpampuha88@gmail.com>
2025-08-10 17:56:13 +0300

    update index.html

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -36,9 +36,9 @@ export function clearGallery() {
 }
 
 export function showLoader() {
-  document.getElementById('loader').classList.remove('hidden');
+  document.getElementById('.loader').classList.remove('hidden');
 }
 
 export function hideLoader() {
-  document.getElementById('loader').classList.add('hidden');
+  document.getElementById('.loader').classList.add('hidden');
 }
