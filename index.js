e48daa9b7a23b6713bfa99a96f63f37d170f6720
IvanVsemDobra <ivanpampuha88@gmail.com>
2025-08-12 10:44:30 +0000

    Deploying to gh-pages from @ IvanVsemDobra/goit-js-hw-11@a737d341b4a2710dde2cf43db88b1e7f2a46c81e 🚀

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import{a as d,S as p,i as s}from"./assets/vendor-Dlc6jZjm.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const f="51715222-245823a157b91ecf18a5e059a",m="https://pixabay.com/api/";async function y(i){const r={key:f,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0};return(await d.get(m,{params:r})).data}let g=new p(".gallery a",{captionsData:"alt",captionDelay:250});const c=document.querySelector(".gallery");function h(i){const r=i.map(({webformatURL:o,largeImageURL:n,tags:e,likes:t,views:a,comments:l,downloads:u})=>`
+import{a as d,S as p,i as s}from"./assets/vendor-Dlc6jZjm.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const f="51715222-245823a157b91ecf18a5e059a",m="https://pixabay.com/api/";async function y(i,r=1,o=40){try{const a={key:f,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0,page:r,per_page:o};return(await d.get(m,{params:a})).data}catch(a){throw console.error("Помилка при завантаженні зображень:",a),a}}let g=new p(".gallery a",{captionsData:"alt",captionDelay:250});const c=document.querySelector(".gallery");function h(i){const r=i.map(({webformatURL:o,largeImageURL:a,tags:e,likes:t,views:n,comments:l,downloads:u})=>`
         <li class="gallery-item">
-          <a href="${n}">
+          <a href="${a}">
             <img src="${o}" alt="${e}" loading="lazy" />
           </a>
           <div class="info">
             <p><b>Likes:</b> ${t}</p>
-            <p><b>Views:</b> ${a}</p>
+            <p><b>Views:</b> ${n}</p>
             <p><b>Comments:</b> ${l}</p>
             <p><b>Downloads:</b> ${u}</p>
           </div>
