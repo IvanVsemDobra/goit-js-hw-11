a737d341b4a2710dde2cf43db88b1e7f2a46c81e
VsemDobra <ivanpampuha88@gmail.com>
2025-08-12 13:44:05 +0300

    update pixabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 
-const API_KEY = "51715222-245823a157b91ecf18a5e059a"; 
+const API_KEY = "51715222-245823a157b91ecf18a5e059a";
 const BASE_URL = "https://pixabay.com/api/";
 
-export async function getImagesByQuery(query) {
-  const params = {
-    key: API_KEY,
-    q: query,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: true,
-  };
+export async function getImagesByQuery(query, page = 1, perPage = 40) {
+  try {
+    const params = {
+      key: API_KEY,
+      q: query,
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+      page,
+      per_page: perPage,
+    };
 
-  const response = await axios.get(BASE_URL, { params });
-  return response.data;
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    console.error("Помилка при завантаженні зображень:", error);
+    throw error;
+  }
 }
